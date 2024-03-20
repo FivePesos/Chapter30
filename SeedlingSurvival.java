08ed876674cb212ac73d35ec2150a002a74967d2
SingCow <75522316+FivePesos@users.noreply.github.com>
2024-03-20 18:47:07 +0800

    Update SeedlingSurvival.java

--- SeedlingSurvival.java
+++ SeedlingSurvival.java
@@ -1,28 +1,24 @@
-// Online Java Compiler
-// Use this editor to write, compile and run your Java code online
 import java.util.Random;
 class SeedlingSurvival {
     public static void main(String[] args) {
         Random rand = new Random();
         double percentage = 0;
         int alive = 0;
-        String isAlive = "Alive";
         
         for(int i = 1; i <= 100; i++){
             System.out.println("Season: "+ i);
             for(int j = 1; j <= 10; j++){
-                int random = rand.nextInt(10 - 1 + 1) + 1;
+                int random = rand.nextInt(10 - 1 + 1) + 1; // Setting Minimum and Maximum equation according to programmedlessons.org
+                String isAlive = "Dead";
                 if(random < 10){
                     isAlive = "Alive";
                     alive++;
                 }
-                else
-                    isAlive = "Dead";
                 System.out.println("Value of rolled dice is " + random + ". Plant " + j + " is " + isAlive);
             }
         }
         percentage = (alive / 100.0) * 10.0;
-        System.out.println("The average of alive plants is " + percentage + "%");
+        System.out.printf("\nThe average of alive plants is %.2f%%", percentage);
     
     }
 }
